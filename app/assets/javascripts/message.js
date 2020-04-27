0b7c83092b174c520cb640f0cb609ee325d254e5
kentomiyazawa <kento4241@yahoo.co.jp>
2020-04-27 15:34:54 +0900

    自動更新機能作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
   function buildHTML(message){
     if (message.image) {
       var html = 
-        `<div class="chat-main__list__message">
+        `<div class="chat-main__list__message" data-message-id=${message.id}>
           <div class="chat-main__list__message__info">
             <div class="chat-main__list__message__info__name">
               ${message.user_name}
@@ -22,7 +22,7 @@ $(function(){
       return html;
     } else {
       var html = 
-        `<div class="chat-main__list__message">
+        `<div class="chat-main__list__message" data-message-id=${message.id}>
           <div class="chat-main__list__message__info">
             <div class="chat-main__list__message__info__name">
               ${message.user_name}
@@ -40,6 +40,28 @@ $(function(){
         return html;
     };
   }
+  var reloadMessages = function(){
+    var last_message_id = $('.chat-main__list__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__list').append(insertHTML);
+        $('.chat-main__list').animate({ scrollTop: $('.chat-main__list')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      alert('erroe');
+    });
+   };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -65,5 +87,8 @@ $(function(){
     $('form')[0].reset();
     $('.form__send--btn').prop('disabled',false);
    })
-})
+  })
+   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
